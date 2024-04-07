4f8fdb5480589231f5ec55abfc7b772f8ec1df72
Deus <119631661+OldKokoroz@users.noreply.github.com>
2024-04-07 20:07:07 +0300

    Update

--- decrypter.py
+++ decrypter.py
@@ -2,25 +2,33 @@ import os
 from cryptography.fernet import Fernet
 
 
-files_dict = {}
-files = []
-os.chdir("C:/")
+class Decrypter:
+    def __init__(self,
+                secret_key,
+                files_dict,
+                files
+                ) -> None:
+        
+        self.secret_key = secret_key
+        self.files_dict = files_dict
+        self.files = files
 
-secret_key = ""
+    def decrypt(self) -> None:
+        for _ in self.files:
+            if _ == '.DS_Store':
+                continue
+
+            if _ in self.files_dict:
+                os.rename(_, self.files_dict[_])
+
+            with open(_, "rb") as the_file:
+                contents = the_file.read()
+
+            contents_decrypted = Fernet(self.secret_key).decrypt(contents)
+
+            with open(_, "wb") as the_file:
+                the_file.write(contents_decrypted)
 
 
-def decrypt():
-    for _ in files:
-        if _ == '.DS_Store':
-            continue
-
-        if _ in files_dict:
-            os.rename(_, files_dict[_])
-
-        with open(_, "rb") as the_file:
-            contents = the_file.read()
-
-        contents_decrypted = Fernet(secret_key).decrypt(contents)
-
-        with open(_, "wb") as the_file:
-            the_file.write(contents_decrypted)
+start = Decrypter()
+start.decrypt()
